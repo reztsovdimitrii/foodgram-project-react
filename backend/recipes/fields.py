4e368b84fd175f1cf8d2e620cf98aab873e8af2e
Dmitrii Reztsov <reztsovdimitrii@gmail.com>
2022-10-30 19:47:13 +0300

    API

--- backend/recipes/fields.py
+++ backend/recipes/fields.py
@@ -9,7 +9,6 @@ class HexColorField(models.CharField):
         kwargs.setdefault('max_length', 7)
         super().__init__(*args, **kwargs)
         self.validators.append(
-            #    regex=r'^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$',
             validators.RegexValidator(
                 regex=r'#([a-fA-F0-9]{6})',
                 message=_('Введите корректное значение HEX кода цвета'),
